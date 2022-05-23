ba225e3f227e96f48dcfe8e05e485ef5b5bb8b05
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-05-23 06:40:01 +0600

    user email added

--- index.js
+++ index.js
@@ -33,7 +33,31 @@ function verifyJWT(req, res, next) {
 }
 
 
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("Electrific").collection("users");
 
+    
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ result, token });
+    });
+    
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Electrific Server Running");
