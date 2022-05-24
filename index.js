c8f86adb3df2df4414c3e07c0e4ed40c0f8a5063
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-05-24 15:45:46 +0600

    delete specific product

--- index.js
+++ index.js
@@ -164,6 +164,14 @@ async function run() {
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     });
+
+    // Delete specific product
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
